6cb98dd4078dc56b031d581a56003fc8ce3a1691
diogolhc <64315652+diogolhc@users.noreply.github.com>
2022-03-08 21:36:10 +0000

    readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Computer Networks - Network configuration & Download application
+
+
+## Description  
+
+The goal of this project was to configure a network in the
+[lab](https://sigarra.up.pt/feup/pt/instal_geral.espaco_view?pv_id=74614)
+with 2 VLANs using 3 computers, 1 switch, 1 Cisco router and connection to
+the internet to download a file with an implemented download application.
+
+
+This project was developed by Diogo Luís Henriques Costa 
+(up201906731@edu.fe.up.pt) and Francisco José Barbosa Marques 
+Colino (up201905405@edu.fe.up.pt) for
+[RCOM 2021-22](https://sigarra.up.pt/feup/en/UCURR_GERAL.FICHA_UC_VIEW?pv_ocorrencia_id=484435).
+
+
+## Products
+
+The project final report can be found at [docs/report.pdf](docs/report.pdf) 
+and the source code of the download application at [src](src).  
--- network/tux.sh
+++ network/tux.sh
@@ -3,8 +3,6 @@ table_num=$1
 tuxy_num=$2
 echo "Table: $table_num"
 echo "Computer: $tuxy_num"
-ifconfig eth0 down
-ifconfig eth1 down
 systemctl restart networking
 ifconfig eth0 down
 ifconfig eth1 down
@@ -12,16 +10,14 @@ if [ $tuxy_num -eq 2 ]
 then
     ifconfig eth0 up
     ifconfig eth0 172.16."$table_num"1.1/24
-    route add default gw 172.16."$table_num"1.1 eth0
     ip route add 172.16."$table_num"0.0/24 via 172.16."$table_num"1.253 dev eth0
-    route add default gw 172.16."$table_num"1.254
+    route add default gw 172.16."$table_num"1.254 eth0
 elif [ $tuxy_num -eq 3 ]
 then
     ifconfig eth0 up
     ifconfig eth0 172.16."$table_num"0.1/24
-    route add default gw 172.16."$table_num"0.1 eth0
     ip route add 172.16."$table_num"1.0/24 via 172.16."$table_num"0.254 dev eth0
-    route add default gw 172.16."$table_num"0.254
+    route add default gw 172.16."$table_num"0.254 eth0
 elif [ $tuxy_num -eq 4 ]
 then
     ifconfig eth0 up
@@ -29,10 +25,9 @@ then
     route add default gw 172.16."$table_num"0.254 eth0
     ifconfig eth1 up
     ifconfig eth1 172.16."$table_num"1.253/24
-    route add default gw 172.16."$table_num"1.253 eth1
+    route add default gw 172.16."$table_num"1.254 eth1
     echo 1 > /proc/sys/net/ipv4/ip_forward
     echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
-    route add default gw 172.16."$table_num"1.254
 else
     echo "Bad input"
 fi
